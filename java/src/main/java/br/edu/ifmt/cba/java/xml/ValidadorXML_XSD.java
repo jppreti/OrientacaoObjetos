ce466f29de471c47191269774a59fd2d5032b391
jppreti <jppreti@gmail.com>
2016-06-11 08:57:00 -0400

    Exemplo de validador de XML com XSD.

---
+++ java/src/main/java/br/edu/ifmt/cba/java/xml/ValidadorXML_XSD.java
@@ -0,0 +1,35 @@
+package br.edu.ifmt.cba.java.xml;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.xml.sax.SAXException;
+
+public class ValidadorXML_XSD {
+
+	public static void main(String[] args) throws SAXException, IOException {
+		URL schemaFile = new URL("http://java.sun.com/xml/ns/j2ee/web-app_2_4.xsd");
+		Source xmlFile = new StreamSource(new File("../../DesenvolvimentoWeb/web/src/main/webapp/WEB-INF/web.xml"));
+		SchemaFactory schemaFactory = SchemaFactory
+		    .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = schemaFactory.newSchema(schemaFile);
+		Validator validator = schema.newValidator();
+		try {
+		  validator.validate(xmlFile);
+		  System.out.println(xmlFile.getSystemId() + " is valid");
+		} catch (SAXException e) {
+		  System.out.println(xmlFile.getSystemId() + " is NOT valid");
+		  System.out.println("Reason: " + e.getLocalizedMessage());
+		}
+	}
+	
+}
